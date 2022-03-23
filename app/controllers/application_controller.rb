b56f05ee5ea12410efad0ba8f7eafdebfeeb7fbd
April Skrine <april.skrine@gmail.com>
2022-03-23 11:20:55 -0500

    commit tree 7323f689fefa78f706a3ea4006549207c9d0cb01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def render_invalid(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def not_found(error)
+    render json: {error: "#{error.model} not found"}, status: 401
+  end
+
 end
